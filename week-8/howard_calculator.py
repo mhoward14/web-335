7946190ffa612c96e8faa21e9aa0cca27302a0fa
Matthew Howard <mhoward14@icloud.com>
2018-06-24 17:56:18 -0600

    Python_Calculator

---
+++ week-8/howard_calculator.py
@@ -0,0 +1,25 @@
+first_name = 'Matthew'
+last_name = 'Howard'
+print(first_name+''+last_name)
+
+
+even_numbers = [2,4,6,8,10]
+
+odd_numbers = [1,3,5,7,9]
+
+number01 = 4
+
+number02 = 7
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def divide(x, y):
+    return x / y
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
