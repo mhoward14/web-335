c80a477c423c2968e4e3e73ef1f73af28cc935f5
Matthew Howard <mhoward14@icloud.com>
2018-06-28 17:17:35 -0600

    Python Exercise-9.2

---
+++ week-9/howard-user-service.py
@@ -0,0 +1,24 @@
+import pymongo 
+from pymongo import MongoClient
+import pprint
+
+client = MongoClient('localhost', 27017)
+db = client.web335
+employee_id = "0000002"
+pprint.pprint(db.users.find_one({"employee_id": employee_id}))
+
+import datetime
+
+client = MongoClient('localhost',27017)
+db = client.web335
+user = {
+    "first_name": "Claude",
+    "last_name": "Debussy",
+    "email": "cdebussy@me.com",
+    "employee_id": "0000008",
+    "date_created": datetime.datetime.utcnow()
+}
+
+user_id = db.users.insert_one(user).inserted_id
+print(user_id)
+pprint.pprint(db.users.find_one({"employee_id": "0000008"}))
